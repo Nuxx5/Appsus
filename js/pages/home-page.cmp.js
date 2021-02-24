14c377dc8e0563c20de7bc36dd2aa6c18d18945d
Ran Porath <ranporath@gmail.com>
2021-02-24 14:56:31 +0200

    homepage visuals

--- js/pages/home-page.cmp.js
+++ js/pages/home-page.cmp.js
@@ -1,1 +1,8 @@
-export default {}
+export default {
+    template:`
+    <section class="home app-main">
+        <h1 class="home-text">Appsus</h1>
+        <p>Welcome to your new digital orgaziner!</p>
+    </section>
+    `
+}
