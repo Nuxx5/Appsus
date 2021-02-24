a4d307b321165ebae6d70ea6f7dd1950bc4864d4
Ran Porath <ranporath@gmail.com>
2021-02-24 11:52:14 +0200

    routes update

---
+++ js/routes.js
@@ -0,0 +1,35 @@
+import homePage from './pages/home-page.cmp.js'
+import keepApp from './apps/keep/pages/book-add.cmp.js'
+import keepDetails from './apps/keep/pages/keep-details.cmp.js'
+import emailApp from './apps/mail/pages/email-app.cmp.js'
+import emailDetails from './apps/mail/pages/email-details.cmp.js'
+import about from './pages/about.cmp.js'
+
+const routes = [
+    {
+        path: '/',
+        component: homePage,
+    },
+    {
+        path: '/about',
+        component: about
+    },
+    {
+        path: '/keep',
+        component: keepApp
+    },
+    {
+        path: '/keep/:noteId',
+        component: keepDetails
+    },
+    {
+        path: '/mail',
+        component: emailApp
+    },
+    {
+        path: '/mail/:emailId',
+        component: emailDetails
+    },
+]
+
+export const myRouter = new VueRouter({ routes })
