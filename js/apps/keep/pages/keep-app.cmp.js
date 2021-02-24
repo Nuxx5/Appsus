9bc258f3310db432f5d83cc5ea5ab473feb609f9
Bari Avni <avnibari@gmail.com>
2021-02-24 19:07:38 +0200

    general changes

--- js/apps/keep/pages/keep-app.cmp.js
+++ js/apps/keep/pages/keep-app.cmp.js
@@ -1,7 +1,7 @@
-import { keepService } from '../services/keep.service.js'
-import keepFilter from '../cmps/keep-filter.cmp.js'
-import keepList from '../cmps/keep-list.cmp.js'
-import keepDetails from '../pages/keep-details.cmp.js'
-import keepEdit from '../pages/keep-edit.cmp.js'
+// import { keepService } from '../services/keep.service.js'
+// import keepFilter from '../cmps/keep-filter.cmp.js'
+// import keepList from '../cmps/keep-list.cmp.js'
+// import keepDetails from '../pages/keep-details.cmp.js'
+// import keepEdit from '../pages/keep-edit.cmp.js'
 
 export default { }
