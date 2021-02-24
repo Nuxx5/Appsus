3c280f6abab320d907b35c212bcb9725443b211e
Ran Porath <ranporath@gmail.com>
2021-02-24 17:54:13 +0200

    general changes

---
+++ js/apps/keep/cmps/keep-list.cmp.js
@@ -0,0 +1,31 @@
+import keepPreview from './keep-preview.cmp.js'
+
+export default {
+    props: ['notes'],
+    template: `
+    <ul class="note-list">
+        <li v-for="note in notes" :key="note.id" class="note-preview-container" >
+            <keep-preview :note="note" @click.native="logId(note.id)" />
+            <div class="btns-container">
+                <!-- <button @click="remove(book.id)">X</button> -->
+                <!-- <button @click="select(book)">Details</button> -->
+                <!-- <router-link :to="'/keep/'+keep.id">Details</router-link> -->
+            </div>
+        </li>
+    </ul>
+    `,
+    methods: {
+        remove(noteId) {
+            this.$emit('remove', noteId)
+        },
+        select(note) {
+            this.$emit('selected', note)
+        },
+        logId(noteId) {
+            console.log('Id is', noteId);
+        }
+    },
+    components:{
+        keepPreview
+    }
+}
---
+++ js/apps/keep/services/keep.service.js
@@ -0,0 +1,47 @@
+import { utilService } from '../../../services/util.service.js'
+import { storageService } from '../../../services/async-storage.service.js'
+
+const NOTES_KEY = 'notes'
+const gNotes = _createNotes()
+
+export const keepService = {
+    _createNotes
+}
+
+function _createNotes() {
+    let notes = utilService.loadFromStorage(NOTES_KEY)
+    if (!notes || !notes.length) {
+        notes = [
+            {
+                id: utilService.makeId(),
+                type: "NoteTxt",
+                isPinned: true,
+                info: {
+                    txt: "Fullstack Me Baby!"
+                }
+            },
+            {
+                id: utilService.makeId(),
+                type: "NoteImg",
+                info: {
+                    url: "https://cataas.com/cat/says/hello%20world!",
+                    title: "Me Cat Mi"
+                },
+                style: {
+                    backgroundColor: "#00d"
+                }
+            },
+            {
+                id: utilService.makeId(),
+                type: "NoteTodos", info: {
+                    label: "How was it:", todos: [
+                        { txt: "Do that", doneAt: null },
+                        { txt: "Do this", doneAt: 187111111 }
+                    ]
+                }
+            }
+        ]
+        utilService.saveToStorage(NOTES_KEY, notes)
+    }
+    return notes
+}
